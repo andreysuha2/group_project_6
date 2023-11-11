3aa21f044ad4154eec3de3cffbbec5669f757c0f
andreysuha2 <37827400+andreysuha2@users.noreply.github.com>
2023-11-11 17:21:53 +0200

    Revert "new modify notes"

--- app/Record/Record.py
+++ app/Record/Record.py
@@ -1,14 +1,12 @@
 from typing import Optional
 from datetime import datetime
-from app.Fields import NameField, PhoneField, BirthdayField, MailField, AdressField
+from app.Fields import NameField, PhoneField, BirthdayField
 
 class Record:
-    def __init__(self, name: NameField, phones: list[PhoneField] = [], birthday: BirthdayField = None, mails: list[MailField] = [], adress: AdressField = None ) -> None:
+    def __init__(self, name: NameField, phones: list[PhoneField] = [], birthday: BirthdayField = None) -> None:
         self.name = name
         self.phones = phones
         self.birthday = birthday
-        self.mails = mails
-        self.adress = adress
 
     def __contains__(self, item):
         if item in self.name:
@@ -17,14 +15,7 @@ class Record:
             return bool(list(filter(lambda phone: item in phone, self.phones)))
         
     def __str__(self) -> str:
-        # return f"{self.name.value}: {'|'.join([ phone.value for phone in self.phones ])}"
-        mails = '; '.join(m.value for m in self.mails)
-        return f"Contact: {self.name.value};\
- phones: {'; '.join(p.value for p in self.phones)}\
-{'; Birthday '+ str(self.birthday.value) if self.birthday else ''}\
-{'; To birthday: '+str(Record.days_to_birthday(self))+' days' if self.birthday else ''}\
-{'; Adress: '+ str(self.adress.value) if self.adress else ''}\
-{'; Mail: '+mails if len(mails)>0 else '' }"
+        return f"{self.name.value}: {'|'.join([ phone.value for phone in self.phones ])}"
 
     def __repr__(self) -> str:
         return str(self)
@@ -44,25 +35,6 @@ class Record:
             self.phones.append(phone)
             return True
         return False
-
-    def add_mail(self, mail: MailField) -> None:
-        if mail not in self.mails:
-            self.mails.append(mail)
-            return True
-        return False
-
-    def add_adress(self, adress: AdressField) -> None:
-        if not self.adress:
-            self.adress = adress
-            return True
-        return False
-
-    def add_birthday(self, birthday: BirthdayField) -> None:
-        if  not self.birthday:
-            self.birthday = birthday
-            return True
-        return False
-
 
     def remove_phone(self, searching_phone: PhoneField) -> None:
         if searching_phone in self.phones:
